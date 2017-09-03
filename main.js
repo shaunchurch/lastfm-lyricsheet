0040a973b7f5c8c57bb88e3e8d7a691d76b70f7a
Shaun Church <shaun@shaunchurch.com>
2017-09-03 14:17:53 +0100

    Show errors instead of crashing all the time

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const path = require('path')
-const url = require('url')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 500, height: 750,
+    width: 500,
+    height: 750,
     alwaysOnTop: true,
     transparent: true,
     toolbar: true,
@@ -17,34 +18,35 @@ function createWindow () {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
-    app.quit() // quit app, not just window
+    app.quit(); // quit app, not just window
   }
-})
+});
 
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) {
-    createWindow() // create on dock click
+    createWindow(); // create on dock click
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
@@ -52,7 +54,7 @@ const config = require('./config.json');
 const Genius = require('genius-api');
 const LastFmNode = require('lastfm').LastFmNode;
 const Xray = require('x-ray');
-const { ipcMain } = require('electron')
+const { ipcMain } = require('electron');
 
 const genius = new Genius(config.geniusClientAccessToken);
 const lastfm = new LastFmNode({
@@ -62,25 +64,34 @@ const lastfm = new LastFmNode({
 });
 const xray = Xray();
 const trackStream = lastfm.stream(config.lastfmUsername);
-
-ipcMain.on('asynchronous-message', (event, arg) => {
-  event.sender.send('asynchronous-reply', 'ses')
-})
+const errorMsg = "We can't find that one. Sorry!";
 
 ipcMain.on('synchronous-message', (event, arg) => {
-  event.returnValue = 'sause'
-})
-
-trackStream.on('nowPlaying', function(track) {
-  genius.search(track.name + ' ' + track.artist['#text']).then(function(response) {
-    xray(response.hits[0].result.url, '.lyrics@html')(function(err, body) {
-      mainWindow.webContents.send('new-track', {
-        lyrics: body,
-        artist: track.artist['#text'],
-        title: track.name
-      });
-    });
-  });
+  event.returnValue = 'pong';
 });
 
+const handleStreamingTrack = track => {
+  track.artist = track.artist['#text']; // dmo
+  const query = `${track.artist} ${track.name}`;
+  console.log('Searching for', query);
+  genius.search(query).then(res => {
+    let url = res.hits.length > 0 ? res.hits[0].result.url : null;
+    if (!url) return updateWindow(errorMsg, track);
+    console.log('Scraping', url);
+    xray(url, '.lyrics@html')((err, body) => {
+      if (err) return updateWindow(errorMsg, track);
+      updateWindow(body, track);
+    });
+  });
+};
+
+const updateWindow = (body, track) => {
+  mainWindow.webContents.send('new-track', {
+    lyrics: body,
+    artist: track.artist,
+    title: track.name
+  });
+};
+
+trackStream.on('nowPlaying', handleStreamingTrack);
 trackStream.start();
