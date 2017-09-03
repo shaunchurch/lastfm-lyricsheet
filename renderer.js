bda03cbde9d4176f569d76bf488f69025c92818c
Shaun Church <shaun@shaunchurch.com>
2017-09-03 15:33:28 +0100

    Prevent Genius links being clickable

--- renderer.js
+++ renderer.js
@@ -3,12 +3,27 @@
 // All of the Node.js APIs are available in this process.
 
 // In renderer process (web page).
-const {ipcRenderer} = require('electron')
-console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+const { ipcRenderer } = require('electron');
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
 
 ipcRenderer.on('new-track', (event, arg) => {
   window.document.body.innerHTML = '';
-  window.document.body.innerHTML = '<strong>' + arg.artist + ' - ' + arg.title + '</strong><br />' + arg.lyrics;
+  window.document.body.innerHTML =
+    '<strong>' +
+    arg.artist +
+    ' - ' +
+    arg.title +
+    '</strong><br />' +
+    arg.lyrics +
+    '<br /><small>' +
+    new Date(Date.now()).toLocaleString() +
+    '</small>';
   window.document.title = arg.artist + ' - ' + arg.title + ' lyrics';
-})
-ipcRenderer.send('asynchronous-message', 'ping')
+  var anchors = document.getElementsByTagName('a');
+  for (var i = 0; i < anchors.length; i++) {
+    anchors[i].onclick = function() {
+      return false;
+    };
+  }
+});
+ipcRenderer.send('asynchronous-message', 'ping');
